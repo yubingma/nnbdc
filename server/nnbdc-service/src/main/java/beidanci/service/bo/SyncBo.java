1ecbbc6133d9f371acb0ed16f87242b05830bae0
myb <mmyybb3000@gmail.com>
2025-12-28 12:50:09 +0800

    refactor(sync): 调整SyncBo类中的导入语句顺序
    - 将JdbcTemplate导入语句移至其他Spring相关导入之后
    - 保持代码风格一致性和可读性
    - 遵循项目导入语句排序规范

--- server/nnbdc-service/src/main/java/beidanci/service/bo/SyncBo.java
+++ server/nnbdc-service/src/main/java/beidanci/service/bo/SyncBo.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionDefinition;
