551fee65bd7d7b9f2d84e3c924a04fd2f691bd38
myb <mmyybb3000@gmail.com>
2026-02-19 13:23:04 +0800

    fix

--- app/lib/util/sync.dart
+++ app/lib/util/sync.dart
@@ -557,9 +557,9 @@ Pair<List<Map<String, dynamic>>, List<Map<String, dynamic>>> mergeChanges(
     }
   }
 
-  // 去除重复项，确保每个 ID 只出现一次, 减小数据库操作次数和网络包体积
-  localToBackend = List.from({for (var log in localToBackend) '${log['tblName']}|${log['recordId']}': log}.values);
-  backToLocalLogs = List.from({for (var log in backToLocalLogs) '${log['tblName']}|${log['recordId']}': log}.values);
+  // 去除重复项，确保每个记录的同一种操作只出现一次, 减小数据库操作次数和网络包体积
+  localToBackend = List.from({for (var log in localToBackend) '${log['tblName']}|${log['recordId']}|${log['operate']}': log}.values);
+  backToLocalLogs = List.from({for (var log in backToLocalLogs) '${log['tblName']}|${log['recordId']}|${log['operate']}': log}.values);
 
   return Pair(localToBackend, backToLocalLogs);
 }
