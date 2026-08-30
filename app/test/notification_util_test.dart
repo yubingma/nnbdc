00bd0f7efbace48f16c348ab51b47adc7e4a065e
myb <mmyybb3000@gmail.com>
2026-08-30 11:18:32 +0800

    refactor: replace MockNotificationsPlatform with MethodChannel mock in notification utility tests

--- app/test/notification_util_test.dart
+++ app/test/notification_util_test.dart
@@ -1,60 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:nnbdc/util/notification_util.dart';
 import 'package:nnbdc/util/prefs.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:timezone/data/latest_all.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:flutter_local_notifications_platform_interface/flutter_local_notifications_platform_interface.dart';
-import 'package:plugin_platform_interface/plugin_platform_interface.dart';
-
-class MockNotificationsPlatform extends FlutterLocalNotificationsPlatform
-    with MockPlatformInterfaceMixin {
-  @override
-  Future<bool?> initialize(
-    InitializationSettings? settings, {
-    dynamic onDidReceiveNotificationResponse,
-    dynamic onDidReceiveBackgroundNotificationResponse,
-  }) async =>
-      true;
-
-  @override
-  Future<void> cancel({required int id, String? tag}) async {}
-
-  @override
-  Future<void> cancelAll() async {}
-
-  @override
-  Future<void> show({
-    required int id,
-    String? title,
-    String? body,
-    NotificationDetails? notificationDetails,
-    String? payload,
-  }) async {}
-
-  @override
-  Future<void> zonedSchedule({
-    required int id,
-    String? title,
-    String? body,
-    required tz.TZDateTime scheduledDate,
-    required NotificationDetails notificationDetails,
-    AndroidScheduleMode? androidScheduleMode,
-    DateTimeComponents? matchDateTimeComponents,
-    String? payload,
-  }) async {}
-}
 
 void main() {
+  AndroidFlutterLocalNotificationsPlugin.registerWith();
   TestWidgetsFlutterBinding.ensureInitialized();
 
   setUpAll(() {
     tz.initializeTimeZones();
     tz.setLocalLocation(tz.getLocation('Asia/Shanghai'));
-    FlutterLocalNotificationsPlatform.instance = MockNotificationsPlatform();
+
+    const channel = MethodChannel('dexterous.com/flutter/local_notifications');
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(channel, (MethodCall methodCall) async {
+      if (methodCall.method == 'initialize') {
+        return true;
+      }
+      return null;
+    });
 
     TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
         .setMockMethodCallHandler(
